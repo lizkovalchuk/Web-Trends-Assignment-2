aa14a7e3ab2f1769a33b1572c37f12326c7ccdf1
Liz Kovalchuk <liz.kovalchuk@gmail.com>
2018-06-28 21:10:44 -0400

    loop working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 //import { CommonModule } from '@angular/common';
 import { ClimbComponent } from './climb/climb.component';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent} from './app.component'
+
 
 const routes: Routes = [
   {path: 'climb', component: ClimbComponent},
